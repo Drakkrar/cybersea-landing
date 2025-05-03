d3efd2bdebb6c40522054e4a27c55b9a544f47b5
Francisco Ramos <jramossepulveda3@gmail.com>
2025-05-03 10:32:33 -0600

    feat: update contact view with CTA section and enhance styling

--- src/app/landing/views/contact-view/contact-view.component.ts
+++ src/app/landing/views/contact-view/contact-view.component.ts
@@ -1,9 +1,10 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { CtaSectionComponent } from "../../components/cta-section/cta-section.component";
 
 @Component({
   selector: 'landing-contact-view',
   standalone: true,
-  imports: [],
+  imports: [CtaSectionComponent],
   templateUrl: './contact-view.component.html',
   styleUrl: './contact-view.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
