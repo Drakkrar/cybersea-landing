546e06083d9fa5ff960dd80604f945ae96d390af
Francisco Ramos <jramossepulveda3@gmail.com>
2025-05-03 10:47:18 -0600

    feat: implement contact component and integrate it into the CTA section

---
+++ src/app/landing/components/contact/contact.component.ts
@@ -0,0 +1,11 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'landing-contact-component',
+  standalone: true,
+  imports: [],
+  templateUrl: './contact.component.html',
+  styleUrl: './contact.component.css',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class ContactComponent { }
--- src/app/landing/components/cta-section/cta-section.component.ts
+++ src/app/landing/components/cta-section/cta-section.component.ts
@@ -1,9 +1,10 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ContactComponent } from "../contact/contact.component";
 
 @Component({
   selector: 'landing-cta-section',
   standalone: true,
-  imports: [],
+  imports: [ContactComponent],
   templateUrl: './cta-section.component.html',
   styleUrl: './cta-section.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
