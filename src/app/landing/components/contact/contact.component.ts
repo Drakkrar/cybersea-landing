5d2c1837bdfcf9d25edc1b8d9038fd233f098128
Francisco Ramos <jramossepulveda3@gmail.com>
2025-05-03 11:12:05 -0600

    feat: component connected with service and validators

--- src/app/landing/components/contact/contact.component.ts
+++ src/app/landing/components/contact/contact.component.ts
@@ -1,11 +1,43 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
+import { ContactService } from '../../services/contact.service';
 
 @Component({
   selector: 'landing-contact-component',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ContactComponent { }
+export class ContactComponent implements OnInit { 
+  contactForm!: FormGroup;
+
+  constructor(private fb: FormBuilder, private contactService: ContactService) {}
+
+  ngOnInit(): void {
+    this.initForm()
+  }
+
+  private initForm(): void {
+    this.contactForm = this.fb.group({
+      name: ['', [Validators.required, Validators.minLength(2)]],
+      email: ['', [Validators.required, Validators.email]],
+      message: ['']
+    });
+  }
+
+  onSubmit(): void {
+    if (this.contactForm.valid) {
+      this.contactService.submitContactForm(this.contactForm.value);
+      this.contactForm.reset();
+    } else {
+      // Mark all fields as touched to trigger validation errors
+      Object.keys(this.contactForm.controls).forEach(key => {
+        const control = this.contactForm.get(key);
+        control?.markAsTouched();
+      });
+    }
+  }
+}
