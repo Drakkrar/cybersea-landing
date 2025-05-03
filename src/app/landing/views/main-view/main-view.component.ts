bb5978378f03c9be59ec542f7826b292af0a5706
Francisco Ramos <jramossepulveda3@gmail.com>
2025-05-03 10:01:28 -0600

    refactor: delete unused imports on components

--- src/app/landing/views/main-view/main-view.component.ts
+++ src/app/landing/views/main-view/main-view.component.ts
@@ -5,14 +5,11 @@ import { HeroSectionComponent } from "../../components/hero-section/hero-section
 import { InformationSectionComponent } from "../../components/information-section/information-section.component";
 import { BenefitsSectionComponent } from "../../components/benefits-section/benefits-section.component";
 import { SocialproofSectionComponent } from "../../components/socialproof-section/socialproof-section.component";
-import { FaqSectionComponent } from "../../components/faq-section/faq-section.component";
-import { CtaSectionComponent } from "../../components/cta-section/cta-section.component";
-import { AboutusSectionComponent } from "../../components/aboutus-section/aboutus-section.component";
 
 @Component({
   selector: 'landing-main-view',
   standalone: true,
-  imports: [FooterComponent, HeaderComponent, HeroSectionComponent, InformationSectionComponent, BenefitsSectionComponent, SocialproofSectionComponent, FaqSectionComponent, CtaSectionComponent, AboutusSectionComponent],
+  imports: [FooterComponent, HeaderComponent, HeroSectionComponent, InformationSectionComponent, BenefitsSectionComponent, SocialproofSectionComponent],
   templateUrl: './main-view.component.html',
   styleUrl: './main-view.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
